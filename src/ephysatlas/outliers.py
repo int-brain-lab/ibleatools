efa0636ea351aac6fa1b894c2b104ab77769abc2
GaelleChapuis <gaelle.chapuis.ibl@gmail.com>
2025-08-19 12:14:07 +0200

    svm score

--- src/ephysatlas/outliers.py
+++ src/ephysatlas/outliers.py
@@ -395,7 +395,7 @@ def get_gamma_svm(X_train, f=0.4):
     return gamma
 
 
-def outlier_score_svm(X_train, X_test, nu=0.2, f=0.4, kernel='rbf'):
+def outlier_score_svm(X_train, X_test, nu=0.2, f=0.4, kernel='rbf', output='prediction'):
     # Step 0 : Filter the train data to remove large outliers
     X_train = X_train[
         np.abs(X_train - np.median(X_train)) <= 5 * iqr(X_train)
@@ -404,8 +404,22 @@ def outlier_score_svm(X_train, X_test, nu=0.2, f=0.4, kernel='rbf'):
     gamma = get_gamma_svm(X_train, f=f)
     model = OneClassSVM(kernel=kernel, gamma=gamma, nu=nu)
     model.fit(X_train.reshape(-1, 1))  # This is slow for high N ; e.g. 29 seconds for 69k samples
-    pred_svm = model.predict(X_test.reshape(-1, 1))
-    return pred_svm
+
+    if output == 'prediction':
+        # Let the model decide what is the predicted output, score is set to 0
+        pred_svm = model.predict(X_test.reshape(-1, 1))
+        score_test = np.zeros(np.shape(pred_svm))
+    elif output == 'score':
+        # Outputting a score instead of directly the SVM prediction
+        scores_train = model.decision_function(X_train.reshape(-1, 1))
+        scores_test = model.decision_function(X_test.reshape(-1, 1))
+        threshold = np.percentile(scores_train, 100 * nu)  # nu fraction as outliers
+        pred_svm = np.where(scores_test >= threshold, 1, -1)
+
+    # map the OneClassSVM output from {1, -1} into {0, 1}
+    pred_svm_01 = np.where(pred_svm == -1, 1, 0)
+
+    return pred_svm_01, score_test
 
 
 def generate_testset(X_train):
@@ -449,7 +463,7 @@ def score_svm_1pid(df_base, df_new, features, mapping,
             train_data = df_train.to_numpy()
             test_data = df_test.to_numpy()
 
-            print(f'Train N: {len(train_data)}, Test N: {len(test_data)}')
+            # print(f'Train N: {len(train_data)}, Test N: {len(test_data)}')
 
             # Outlier → 1
             # Inlier → 0
@@ -457,16 +471,13 @@ def score_svm_1pid(df_base, df_new, features, mapping,
             # score_out = 0 if N pid or N channel too small in training set
             if bool((df_pid.nunique().values[0] >= n_pid) & (df_pid.shape[0] >= min_ch_compute)):
                 if len(train_data) > max_ch_compute:  # Apply KDE method as SVM model.fit is too slow for high N
-                    scoreq_out, _, _ = kde_proba_distribution(train_data,
-                                                              test_data)  # This is a score to be thresholded
+                    scoreq_out, _, _ = kde_proba_distribution(train_data, test_data)  # This is a score to be thresholded
                     scoreq_out = scoreq_out.squeeze()  # TODO this should not be necessary, place in kde_proba_distribution
                     score_out = scoreq_out > p_thresh_kde
                     score_out = score_out.astype(int)
 
                 else:
-                    score_svm = outlier_score_svm(train_data, test_data)
-                    # map the OneClassSVM output from {1, -1} into {0, 1}
-                    score_out = np.where(score_svm == -1, 1, 0)
+                    pred_svm, _ = outlier_score_svm(train_data, test_data)
             else:
                 score_out = np.zeros(test_data.shape)
             # Save into new column
