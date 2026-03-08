2f170488d70eda6b1b82c178114427eed2e51996
Pranav Rai <rpranaviitk@gmail.com>
2026-03-08 17:55:01 +0000

    Added todo

--- src/ephysatlas/features.py
+++ src/ephysatlas/features.py
@@ -1043,6 +1043,7 @@ def csd(data, fs, geometry, bands=None, decimate=10):
         before computing the spectral features.
     """
     data_rs = scipy.signal.decimate(data, decimate, axis=1, ftype="fir")
+    #TODO Change this to include the NP2 geometry as well, and pass it a h parameter.
     data_rs = ibldsp.cadzow.cadzow_np1(data_rs, rank=2, fs=fs, niter=1, fmax=90)
     # Calculate the CSD features
     data_rs = ibldsp.voltage.current_source_density(data_rs, h=geometry)
